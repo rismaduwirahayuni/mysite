37a155d7965dba289e0a4d04f947c331040b6d64
Risma Duwi Rahayuni <rismaduwirahayuni@gmail.com>
2022-02-16 14:18:23 +0700

    tugas1

---
+++ index.js
@@ -0,0 +1,28 @@
+// Buat 4 function bernama tambah, kurang, kali, bagi
+let number1 = 24;
+let number2 = 2;
+
+function tambah() {
+    let result = number1 + number2;
+    console.log(result)
+}
+
+function kurang() {
+    let result = number1 - number2;
+    console.log(result)
+}
+
+function bagi() {
+    let result = number1 / number2;
+    console.log(result)
+}
+
+function kali() {
+    let result = number1 * number2;
+    console.log(result)
+}
+
+tambah();
+kurang();
+bagi();
+kali();
